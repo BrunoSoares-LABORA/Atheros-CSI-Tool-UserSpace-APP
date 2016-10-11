5cd17c638d4e42b0f6d88e825a63ae89a9b328b4
Bruno Soares da Silva <brunosoares.inf@gmail.com>
2016-10-11 16:38:34 -0300

    - Adicionando tratamento de comandos.

--- recvCSIToSocket/main.c
+++ recvCSIToSocket/main.c
@@ -2,7 +2,7 @@
  * =====================================================================================
  *       Filename:  main.c
  *
- *    Description:  Here is an example for receiving CSI matrix 
+ *    Description:  Here is an example for receiving CSI matrix
  *                  Basic CSi procesing fucntion is also implemented and called
  *                  Check csi_fun.c for detail of the processing function
  *        Version:  1.0
@@ -10,7 +10,7 @@
  *         Author:  Yaxiong Xie
  *         Email :  <xieyaxiongfly@gmail.com>
  *   Organization:  WANDS group @ Nanyang Technological University
- *   
+ *
  *   Copyright (c)  WANDS group @ Nanyang Technological University
  * =====================================================================================
  */
@@ -27,10 +27,11 @@
 #include <sys/stat.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <arpa/inet.h>
 #include <errno.h>
-#include <time.h> 
-
+#include <time.h>
+#include <math.h>
 
 #include "csi_fun.h"
 
@@ -49,6 +50,13 @@ void sig_handler(int signo)
         quit = 1;
 }
 
+int enable_keepalive(int sock) {
+    setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, &(int){ 1 }, sizeof(int));
+    setsockopt(sock, IPPROTO_TCP, TCP_KEEPIDLE, &(int){ 1 }, sizeof(int));
+    setsockopt(sock, IPPROTO_TCP, TCP_KEEPINTVL, &(int){ 1 }, sizeof(int));
+    setsockopt(sock, IPPROTO_TCP, TCP_KEEPCNT, &(int){ 10 }, sizeof(int));
+}
+
 int main(int argc, char* argv[])
 {
     /* check usage */
@@ -58,7 +66,7 @@ int main(int argc, char* argv[])
         printf("/**************************************/\n");
         return 0;
     }
-    
+
     int         fd;
     int         i;
     int         total_msg_cnt,cnt;
@@ -66,72 +74,97 @@ int main(int argc, char* argv[])
     u_int16_t   buf_len;
     csi_status = (csi_struct*)malloc(sizeof(csi_struct));
 
-    fd = open_csi_device();
-    if (fd < 0){
-        perror("Failed to open the device...");
-        return errno;
-    }
-    
     /* Open socket server */
     int port = atoi(argv[1]);
     int listenfd = 0, connfd = 0;
     struct sockaddr_in serv_addr;
     char sendBuff[BUFSIZE];
 
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
     memset(&serv_addr, '0', sizeof(serv_addr));
-    memset(sendBuff, '0', sizeof(sendBuff)); 
+    memset(sendBuff, '0', sizeof(sendBuff));
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(port); 
+    serv_addr.sin_port = htons(port);
 
-    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
-    listen(listenfd, 10); 
-    
+    listenfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(listenfd == -1) {
+	printf("Could not create socket\n");
+        return 0;
+    }
+
+    // Set socket reusable
+    if(setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) == -1) {
+	printf("Could not set socket reusable\n");
+	return 0;
+    }
+
+    if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) {
+	printf("Could not bind socket\n");
+	return 0;
+    }
+    if(listen(listenfd, 1) == -1) {
+	printf("Could not listen socket\n");
+	return 0;
+    }
+
     printf("#Receiving data! Press Ctrl+C to quit!\n");
 
     quit = 0;
     total_msg_cnt = 0;
-    
+    char buff_size[50];
+
     while(1) {
+		printf("Waiting for client connection...\n");
 		connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
-		
+		enable_keepalive(connfd);
+		printf("Connection received...\n");
+
+		/* Connection with client oppened, create device reading */
+		fd = open_csi_device();
+		if (fd < 0) {
+			printf("Failed to open the CSI device...\n");
+			close(connfd);
+			continue;
+		}
+
 		while(1) {
-			if (1 == quit) {
-				close(connfd);
-				close(listenfd);
-				close_csi_device(fd);
-				return 0;
-			}
-			
-			/* keep listening to the kernel and waiting for the csi report */
-			cnt = read_csi_buf(buf_addr,fd,BUFSIZE);
+			printf("Waiting for client command...\n");
+			memset(&buff_size[0], 0, sizeof(buff_size));
+			recv(connfd, buff_size, 45, 0);
+			printf("Command received: %s\n", buff_size);
 
-			if (cnt) {
+			if(strcmp("PACKET_REQUEST", buff_size) == 0) {
+				/* keep listening to the kernel and waiting for the csi report */
+				do {
+					cnt = read_csi_buf(buf_addr, fd, BUFSIZE);
+				} while(!cnt);
+
 				total_msg_cnt += 1;
-
-				/* fill the status struct with information about the rx packet */
 				record_status(buf_addr, cnt, csi_status);
 
-				/* 
-				 * fill the payload buffer with the payload
-				 * fill the CSI matrix with the extracted CSI value
-				 */
-				record_csi_payload(buf_addr, csi_status, data_buf, csi_matrix); 
-				
-				
-				printf("Recv %dth msg with rate: 0x%02x | payload len: %d\n",total_msg_cnt,csi_status->rate,csi_status->payload_len);
-				
-				/* log the received data for off-line processing */
-				buf_len = csi_status->buf_len;
-				
-				write(connfd, buf_addr, buf_len); 
+				printf("sending csi packet\n");
+				u_int16_t csi_buf_len = csi_status->buf_len;
+				send(connfd, &csi_buf_len, sizeof(csi_buf_len), 0);
+				send(connfd, buf_addr, csi_buf_len, 0);
+				printf("CSI packet sent\n");
+			} else {
+				printf("Unknown command received, closing connection...\n");
+				break;
 			}
 		}
-	}
-	close(connfd);
-    close_csi_device(fd);
+
+		printf("Connection with client closed, closing devices...\n");
+		close(connfd);
+		close_csi_device(fd);
+
+		if(quit == 1) {
+			printf("Shutdown command received, finishing...\n");
+			break;
+		}
+    }
+
     free(csi_status);
     return 0;
 }
+
